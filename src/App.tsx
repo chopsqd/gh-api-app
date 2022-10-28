49f834e10c2619646534139eaa1b736cf8de99fd
Michael Chop <007micle007@gmail.com>
2022-10-28 16:14:24 +0300

    Added ability to find users

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ type SearchResultType = {
 const App = () => {
     const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null)
     const [users, setUsers] = useState<SearchUserType[]>([])
+    const [tempSearch, setTempSearch] = useState('')
 
     useEffect(() => {
         if (selectedUser) {
@@ -20,27 +21,39 @@ const App = () => {
         }
     }, [selectedUser])
 
-    useEffect(() => {
+    const fetchData = (term: string) => {
         axios
-            .get<SearchResultType>(`https://api.github.com/search/users?q=bruh`)
+            .get<SearchResultType>(`https://api.github.com/search/users?q=${term}`)
             .then(res => setUsers(res.data.items))
+    }
+
+    useEffect(() => {
+        fetchData('Chopsqd')
     }, [])
 
     return <div className="App">
         <div>
             <div>
-                <input placeholder={"search"}/>
-                <button>Find</button>
+                <input
+                    placeholder={"search"}
+                    value={tempSearch}
+                    onChange={(e) => {
+                        setTempSearch(e.currentTarget.value)
+                    }}
+                />
+                <button onClick={() => {
+                    fetchData(tempSearch)
+                }}>Find</button>
             </div>
             <ul>
                 {
                     users.map(user => <li
-                            key={user.id}
-                            className={selectedUser === user ? 'selected' : ''}
-                            onClick={() => {
-                                setSelectedUser(user)
-                            }}
-                        >{user.login}</li>)
+                        key={user.id}
+                        className={selectedUser === user ? 'selected' : ''}
+                        onClick={() => {
+                            setSelectedUser(user)
+                        }}
+                    >{user.login}</li>)
                 }
             </ul>
         </div>
